ed6de7207d28dab24edad70a1cb229e75ef9d194
huyle <ghuy102@gmail.com>
2024-07-07 10:20:46 +0700

    Update saga configuration

--- src/main/java/com/huyle/ms/saga/config/SagaConfig.java
+++ src/main/java/com/huyle/ms/saga/config/SagaConfig.java
@@ -1,7 +1,13 @@
 package com.huyle.ms.saga.config;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
+@ComponentScan(basePackages = "com.huyle.ms.saga")
+@EntityScan(basePackages = "com.huyle.ms.saga.entity")
+@EnableJpaRepositories(basePackages = "com.huyle.ms.repository")
 public class SagaConfig {
 }
